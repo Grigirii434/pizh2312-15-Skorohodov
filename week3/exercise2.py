0c394f1c0aa940c738346848ef4825641730d7d8
Grigirii434 <Grigirii434@gmail.com>
2025-05-14 14:40:52 +0300

    Добавил пример
    чуть похавал, добавил пример

--- week3/exercise2.py
+++ week3/exercise2.py
@@ -6,15 +6,20 @@ class Product:
 
     @property
     def profit(self):
-        return self.retail_price - self.purchase_price
+        return f"Разница: {self.retail_price - self.purchase_price}"
 
     @staticmethod
     def average_price(prices=None):
         if not prices:
             return 0
-        return sum(prices) / len(prices)
+        return f"Средняя цена: {sum(prices) / len(prices)}"
 
     @property
     def information(self):
         return f"Товар: {self.name}, розничная цена: {self.retail_price}, закупочная цена: {self.purchase_price}"
 
+product = Product("Шляпа", 1000, 800)
+#Пример
+print(product.profit)
+print(product.information)
+print(Product.average_price([100, 300, 800]))
